4b14d25d74116aca82a29a487bc39d2aa5d05a26
VinhOP <lyquocvinh03@gmail.com>
2021-11-13 13:39:10 +0700

    add features

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,19 +1,27 @@
-import { Flex,Box,Input,Button,Text } from "@chakra-ui/react"
+import { Flex,Box,Input,Button,Text,Textarea } from "@chakra-ui/react"
 import { AiFillLike,AiFillCamera,AiFillSmile,AiOutlineFileGif } from 'react-icons/ai'
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 const icons = [<AiFillCamera size='2em'/>,<AiFillSmile size='2em' />, <AiOutlineFileGif size='2em' />]
 
 
 const ChatApp = () => {
 
-    const [inputData, setInputData] = useState()
+    const [inputData, setInputData] = useState('')
+    const [messages, setMessages] = useState([])
 
     const handleChange = ((e) => {
         const {value} = e.target
         setInputData(value)
     })
 
+    const handleSend = (() => {
+        setMessages([...messages, inputData])
+    })
+
+    useEffect(() => {
+        console.log(messages)
+    }, [messages])
     return ( 
         <>
         <Box bg='gray.100'>
@@ -21,11 +29,24 @@ const ChatApp = () => {
                 <Text fontSize='2em' align='center' fontWeight='bold' > Chat App</Text>
             </Flex>
             <Flex justifyContent='space-between' p='3em'>
+
                         {/** --------------------- Chat Box --------------- */}
                 <Flex flexDirection='column' justifyContent='center' w='full' mr='.5em' bg='white'>
-                    <Flex h='30em' border='2px solid' borderColor='gray' borderRadius={7}> adsa </Flex>
+                    <Flex flexDirection='column'
+                    justifyContent='flex-end'
+                    alignItems='flex-end'
+                    h='30em' 
+                    border='2px solid' 
+                    borderColor='gray' 
+                    borderRadius={7}>
+                        {messages && messages.map((mess) => {
+                           return   <Flex bg='blue.200' borderRadius={18} p='.5em 1em' m='.5em 1em'>
+                                        <Text> {mess}</Text> 
+                                    </Flex>
+                        })}
+                    </Flex>
 
-                    <Flex mt='.1em' bg='white' >
+                    <Flex bg='white' >
                         {icons.map((icon) => {
                             return <Button 
                             _focus= {{boxShadow: 'none'}}
@@ -33,8 +54,9 @@ const ChatApp = () => {
                             >{icon}</Button>
                         })}
 
-                        <Input 
-                        type='text' 
+                        <Textarea
+                        cols='1'
+                        resize='none'
                         borderColor='gray' 
                         wordBreak='break-all' 
                         onChange = {handleChange}
@@ -42,10 +64,10 @@ const ChatApp = () => {
                         />
 
                         {inputData? 
-                            <Button _focus= {{boxShadow: 'none'}}> 
-                                <Text fontSize='1.1em'> Send </Text> 
+                            <Button bg='gray.300' borderRadius= {3} _focus= {{boxShadow: 'none'}} onClick={handleSend}> 
+                                <Text> Send </Text> 
                             </Button> : 
-                            <Button> <AiFillLike size='2em' /></Button>}                   
+                            <Button bg='gray.300'> <AiFillLike size='2em' /></Button>}                   
                     </Flex>
                 </Flex>
                         {/** ------------------------------------------------------ */}
