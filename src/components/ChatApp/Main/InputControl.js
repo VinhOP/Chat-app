7a094fdba804e4920f20cd77dd3f504dd72cd477
VinhOP <lyquocvinh03@gmail.com>
2021-11-22 20:03:30 +0700

    fix emoji input

--- src/components/ChatApp/Main/InputControl.js
+++ src/components/ChatApp/Main/InputControl.js
@@ -1,11 +1,11 @@
-import EmojiPicker from "emoji-picker-react";
+
 import { useState,useEffect, useRef, useContext } from "react"
 import { Flex,Textarea,Button,Text } from "@chakra-ui/react";
 import { AiFillSmile,AiFillLike } from "react-icons/ai";
 import { ChatAppContext } from "../../../contexts/ChatAppContext";
 import { useSelector,useDispatch } from "react-redux";
 import { sendMessage } from "../../../features/users";
-
+import InputEmoji from "react-input-emoji"
 
 const InputControl = () => {
 
@@ -16,23 +16,11 @@ const InputControl = () => {
     const [myID, setMyID] = useState(1)
     const [showEmoList, setShowEmoList] = useState(false)
 
-    const textareaRef = useRef(null)
     const dispatch = useDispatch()
 
     useEffect(() => {
         setInputData('')
     }, [userIndex])
-
-    const onEmojiClick = (event, emojiObject) => {
-        const {selectionStart , selectionEnd} = textareaRef.current
-        const newValue = inputData.slice(0, selectionStart) + emojiObject.emoji + inputData.slice(selectionEnd)
-        setInputData(newValue)
-    }
-
-    const handleChange = ((e) => {
-        const { value } = e.target
-        setInputData(value)
-    })
 
     const handleSend = (() => {
         dispatch(sendMessage({
@@ -43,55 +31,36 @@ const InputControl = () => {
         setInputData('')
     })
 
-    const handleKeyPress= ((e) => {
-        if(e.key === 'Enter') 
-        {
-            e.preventDefault();
-            if(inputData) handleSend();
-        }
-     })
-    
-    const handleShowEmo =(() => {
-        setShowEmoList(!showEmoList)
-        textareaRef.current.focus()
+    const handleEnter = ((e) => {
+        inputData && handleSend();
     })
-
     return ( 
         <>
         {userIndex != null && <Flex mt='.5em' alignItems='center'>
-                        <Button onClick={handleShowEmo}> <AiFillSmile /> </Button>
-                        {showEmoList && <Flex>
-                            <EmojiPicker disableSearchBar onEmojiClick={onEmojiClick}/>
-                        </Flex>}
-                   
+                        <Flex w='55em'>
+                            <InputEmoji
+                            onChange = {setInputData}
+                            onEnter = {handleEnter}
+                            value = {inputData}
+                            />
+                        </Flex>
                         
-                        <Textarea
-                        
-                        rows = '1'
-                        overflow = 'hidden'
-                        resize = 'none'
-                        borderColor = 'gray' 
-                        wordBreak ='break-all' 
-                        onChange = {handleChange}
-                        onKeyPress = {handleKeyPress}
-                        value = {inputData}
-                        ref = {textareaRef}
-                        />
-                        
-
-                        {inputData? 
+                        {inputData?
                             <Button 
                             type='submit' 
                             m='0 .5em' 
                             bg='gray.300' 
                             borderRadius= {3} 
                             _focus= {{boxShadow: 'none'}} 
-                            onClick={handleSend}> 
+                            onClick= {handleSend}
+                            > 
                                 <Text> Send </Text> 
                             </Button> : 
-                            <Button m='0 .5em' 
+                            <Button 
+                            m='0 .5em' 
                             bg='gray.300' 
-                            _focus= {{boxShadow: 'none'}}> 
+                            _focus= {{boxShadow: 'none'}}
+                            > 
                                 <AiFillLike size='2em' />
                             </Button>}
                     </Flex>}
